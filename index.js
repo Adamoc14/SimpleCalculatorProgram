f128d5aacc6a3e08748dea4c826b79f10c379ab5
adamoc <oceallaighadam96@gmail.com>
2020-06-08 19:19:31 +0100

    Styling the Calculator

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+// Variable Declarations and Function Definitions
 let state = 0;
-
+let buttons = document.getElementsByClassName('number_button');
 const sum = a => state += a;
 const minus = b => state -= b;
 const mult = c => state *= c;
@@ -7,7 +8,16 @@ const div = d => state /= d;
 const clear = () => state = 0;
 
 
+
+
+// Initialization Methods
 console.log(sum(12))
 console.log(minus(2))
 console.log(mult(3))
 console.log(div(2))
+
+
+buttons.onclick = function(e){
+    console.log(e.target.value);
+}
+
