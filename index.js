6be11b2ea9280d45a0d30f74b324ada70abad82b
adamoc <oceallaighadam96@gmail.com>
2020-06-08 19:35:21 +0100

    Fixing the functionality of the calculator

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 // Variable Declarations and Function Definitions
 let state = 0;
 let buttons = Array.from(document.getElementsByClassName('number_button'));
-console.log(buttons);
+let operational_buttons = [];
+
+
 const sum = a => state += a;
 const minus = b => state -= b;
 const mult = c => state *= c;
 const div = d => state /= d;
 const clear = () => state = 0;
-
-
-
 
 // Initialization Methods
 console.log(sum(12))
@@ -17,9 +16,10 @@ console.log(minus(2))
 console.log(mult(3))
 console.log(div(2))
 
-
+buttons = buttons.filter(button => button.dataset.number !== true ? operational_buttons.push(button) : buttons.push(button));
+console.log(buttons , operational_buttons);
 buttons.map(button => button.onclick = function(e){
     console.log(e);
-    console.log(e.target.value);
+    
 });
 
