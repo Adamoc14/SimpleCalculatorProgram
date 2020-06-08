bec312ed30f0417e6f306faad0bb0515970f9615
adamoc <oceallaighadam96@gmail.com>
2020-06-08 17:31:30 +0100

    Added the functionality

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-alert("hello gerbels");
+let state = 0;
+
+const sum = a => state += a;
+const minus = b => state -= b;
+const mult = c => state *= c;
+const div = d => state /= d;
+const clear = () => state = 0;
+
+
+console.log(sum(12))
+console.log(minus(2))
+console.log(mult(3))
+console.log(divide(2))
